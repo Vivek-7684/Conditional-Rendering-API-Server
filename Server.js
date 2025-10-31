7a79ffdbab5d2a7f865316dea9ccc1943c16d824
Vivek-7684 <vivek.infosware@gmail.com>
2025-10-31 10:41:59 +0530

    Server

---
+++ Server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const cors = require('cors');
+const app = express();
+
+let connection;
+
+app.use(cors());
+
+async function DatabaseSet() {
+    try {
+        connection = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'redhat@123',
+            database: 'sample'
+        });
+
+        return connection;
+    } catch (err) {
+        console.log("Error:", err.message);
+    }
+
+}
+
+DatabaseSet();
+
+app.get("/Product", async (req, res) => {
+    try {
+        const { name, maxPrice, minPrice, category } = req.query;
+
+        let query = "Select * from Product Where 1=1";
+
+        if (name) query += ` AND name LIKE '%${name}%'`;
+
+        if (maxPrice) query += ` AND maxPrice <= ${maxPrice}`;
+
+        if (minPrice) query += ` AND minPrice >= ${minPrice}`;
+
+        if (category) query += ` AND category = '${category}'`;
+
+        const [rows] = await connection.execute(query);
+
+        res.status(200).send(rows);
+
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+
+});
+
+
+
+app.listen("3000", () => {
+    console.log("Server Started at port:3000");
+})
