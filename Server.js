880517b44bf9a02007c3155a2765ec7513dc2792
Vivek-7684 <vivek.infosware@gmail.com>
2025-11-03 13:12:15 +0530

    Validation set

--- Server.js
+++ Server.js
@@ -1,57 +1,91 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql2/promise");
+const cors = require("cors");
+const { fromZodError } =  require("zod-validation-error");
+const { productSchema } = require("./validation");
 const app = express();
 
 let connection;
 
 app.use(cors());
 
+app.use(express.json());
+
 async function DatabaseSet() {
-    try {
-        connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'Redhat@123',
-            database: 'sample'
-        });
+  try {
+    connection = await mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "Redhat@123",
+      database: "sample",
+    });
 
-        return connection;
-    } catch (err) {
-        console.log("Error:", err.message);
-    }
-
+    return connection;
+  } catch (err) {
+    // console.log("Error:", err.message);
+  }
 }
 
 DatabaseSet();
 
 app.get("/Product", async (req, res) => {
-    try {
-        const { name, maxPrice, minPrice, category } = req.query;
+  try {
+    const { name, maxPrice, minPrice, category } = req.query;
 
-        let query = "Select * from sample_product Where 1=1";
+    let query = "Select * from sample_product Where 1=1";
 
-        if (name) query += ` AND name LIKE '%${name}%'`;
+    if (name) query += ` AND name LIKE '%${name}%'`;
 
-        if (maxPrice) query += ` AND maxPrice <= ${maxPrice}`;
+    if (maxPrice) query += ` AND maxPrice <= ${maxPrice}`;
 
-        if (minPrice) query += ` AND minPrice >= ${minPrice}`;
+    if (minPrice) query += ` AND minPrice >= ${minPrice}`;
 
-        if (category) query += ` AND category = '${category}'`;
+    if (category) query += ` AND category = '${category}'`;
 
-        const [rows] = await connection.execute(query);
+    const result = productSchema.safeParse(req.query);
 
-        res.status(200).send(rows);
-
-    }
-    catch (err) {
-        console.log(err.message);
+    if (!result.success) {
+      return res.status(400).send(result.error.format());
     }
 
+    const [rows] = await connection.execute(query);
+
+    if (rows.length === 0) {
+      return res.status(404).send("No Products Found");
+    } else {
+      res.status(200).send(rows);
+    }
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
 });
 
+app.post("/AddProduct", async (req, res) => {
+  try {
+    const { name, maxPrice, minPrice, category } = req.body;
 
+    const result = productSchema.safeParse(req.body);
+
+    console.log(fromZodError(result.error).toString());
+
+    if (!result.success) {
+      return res.status(400).send(fromZodError(result.error).toString());
+    }
+
+    const query = `INSERT INTO sample_product (name, maxPrice, minPrice, category) VALUES (?, ?, ?, ?)`;
+
+    const values = [name, maxPrice, minPrice, category];
+
+    const ProductList = await connection.execute(query, values);
+
+    // console.log(ProductList);
+
+    res.status(201).send("Product Added Successfully");
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
+  }
+});
 
 app.listen("3000", () => {
-    console.log("Server Started at port:3000");
-})
+  console.log("Server Started at port:3000");
+});
---
+++ validation.js
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const productSchema = z.object({
+  name: z.coerce
+    .string("Name must be Alphabet")
+    .min(3, "Aleast 3 Character Alphabet is required")
+    .optional(),
+  maxPrice: z.coerce
+    .number("Max Price must be Number")
+    .min(1000, "Max Price must be at least 1000")
+    .optional(),
+  minPrice: z.coerce
+    .number("Min Price must be Number")
+    .min(500, "Min Price must be at least 500")
+    .optional(),
+  category: z
+    .enum(["Electronics", "Cloth", "Accessories", "Sports"], {
+      message:
+        "Category like Electronics, Cloth, Accessories and Sports are only allowed",
+    })
+    .optional(),
+});
